13524c5d78c0927d973b15ba53567af6a913add4
Timo Moosmann <timo.moosmann@student.uni-tuebingen.de>
2022-04-28 11:16:59 +0200

    Fully implemented a success score for the calibration results. Tests are all passed.

--- src/calibration/success_score.js
+++ src/calibration/success_score.js
@@ -17,6 +17,108 @@ const calcCalibrationScore = ({acc, borderAcc, perfectAcc}) => {
   });
 };
 
-const getCalibrationScoreEvaluation = () => {};
+const evaluateCalibrationScore = ({
+  borderAcc,
+  perfectAcc,
+  borderPrec,
+  relAcc,
+  relPrec,
+  trys
+}) => {
+  const calibrationScore = calcCalibrationScore({
+    acc: relAcc, borderAcc, perfectAcc
+  });
+  const minForGreen = 80;
+  const minForYellow = 65;
+  const minForOrange = 50;
 
-export {calcCalibrationScore};
+  let accScoreColor = 'red';
+  if (calibrationScore.x >= minForGreen && calibrationScore.y >= minForGreen) {
+    accScoreColor = 'green';
+  } else if (
+    calibrationScore.x >= minForYellow && calibrationScore.y >= minForYellow
+  ) {
+    accScoreColor = 'yellow';
+  } else if (
+    calibrationScore.x >= minForOrange && calibrationScore.y >= minForOrange
+  ) {
+    accScoreColor = 'orange';
+  }
+
+  let precStatus = 'bad';
+  let precStatusColor = 'red';
+  if (relPrec.x <= borderPrec.x && relPrec.y <= borderPrec.y) {
+    precStatus = 'good';
+    precStatusColor = 'green';
+  };
+  let proceedBtnActive = true;
+  let message = '';
+  switch(accScoreColor) {
+    case 'red':
+      message += 'Your accuracy is too low to use the program properly.';
+      break;
+    case 'orange':
+      message +=
+        'Your accuracy is just enough to use the program.';
+      break;
+    case 'yellow':
+      message += 'Your accuracy is good enough to use the program.';
+      break;
+    case 'green':
+      message += 'Your accuracy is good.';
+      break;
+  }
+
+  if (precStatusColor === 'red') {
+    message = addToStringWithNewLine(
+      message, 'Your precision is too low to use the program properly.'
+    );
+  }
+
+  if (accScoreColor === 'red' || precStatusColor === 'red') {
+    if (trys.trys > 0) {
+      message = addToStringWithNewLine(
+        message, 'Please calibrate again, or proceed with limited quality.'
+      );
+    } else {
+      proceedBtnActive = false;
+      message = addToStringWithNewLine(message, 'Please calibrate again.');
+    }
+  } else if (accScoreColor === 'orange' || accScoreColor === 'yellow') {
+    message = addToStringWithNewLine(
+      message, 'If you want an better experience, try to calibrate again.'
+    );
+  }
+
+  trys.trys++;
+  return createCalcScoreEvalOut({
+    accScore: calibrationScore,
+    accScoreColor,
+    precStatus,
+    precStatusColor,
+    message,
+    proceedBtnActive
+  });
+};
+
+const addToStringWithNewLine = (str, appendStr) => {
+  return (str.length > 0) ? str + '\n' + appendStr : appendStr;
+}
+
+const createCalcScoreEvalOut = ({
+  accScore,
+  accScoreColor,
+  precStatus,
+  precStatusColor,
+  message,
+  proceedBtnActive
+}) => ({
+  accScore,
+  accScoreColor,
+  precStatus,
+  precStatusColor,
+  message,
+  proceedBtnActive
+});
+
+export {calcCalibrationScore, createCalcScoreEvalOut, evaluateCalibrationScore};
--- test/calibration/success_score.test.js
+++ test/calibration/success_score.test.js
@@ -1,45 +1,5 @@
 import {createPos} from '../../src/data_types.js';
-import {calcCalibrationScore} from '../../src/calibration/success_score.js';
-
-/*
- *
- * Success score for the calibration
- *
- * Give a score from 0% to 100% to the calculated accuracy aftter ones
- * calibration.
- *
- * 50% means: acc.x = 0.06; acc.y = 0,12
- *
- * For the precision one can either fail, or succed
- * Sucession means that the precision is in x direction >= 0.07, and
- *  in y-direction >= 0.08
- *
- * Depending on the score a sentence is displed on a screen that indiacates
- * how good the calibration was. And a color indicates that too.
- *
- * If ones score is higher or equals 50% one can proceed immeditely,
- * but can also retry the calibration.
- * If one scores less than 50% one have to repeat the calibration one time
- * before being able to prceed.
- *
- * Scenarios:
- * X or Y is under 50% or Precision is insufficient
- *   -> score or precision in red
- *   -> The next time one can proceed, but with warning message if criterias
- *      are not fullfilled;
- *   -> Can proceed without warning message, when criterias are fullfilled.
- * 
- * X or Y is 50% or more and Precision is sufficient
- *   -> Precision in green, show: Precision: good
- *   -> 50% - 59%: orange, message: Accuracy is sufficient.
- *   -> 60% to 75%: yellow, message: Good accuracy.
- *   -> 75% to 100%: green, message: Very good accuracy.
- */
-
-const relAccuracyXUnder50 = createPos({x: 0.05, y: 0.13});
-const relAccuracyYUnder50 = createPos({x: 0.05, y: 0.13});
-const relAccuracyXYUnder50 = createPos({x: 0.05, y: 0.13});
-const relAccuracyXY50 = createPos({x: 0.06, y: 0.12});
+import {calcCalibrationScore, createCalcScoreEvalOut, evaluateCalibrationScore} from '../../src/calibration/success_score.js';
 
 const testBorderAcc = createPos({x: 0.06, y: 0.12});
 const testPerfectAcc = createPos({x: 0.03, y: 0.06});
@@ -75,23 +35,6 @@ test('calcCalibrationScore works well', () => {
   })).toEqual(createPos({x: 50, y: 50}));
 });
 
-const inp = {
-  relAcc: createPos({x: 0.1, y: 0.2}),
-  relPrec: createPos({x: 0.1, y: 0.2}),
-  borderPrec: createPos({x: 0.05, y: 0.1}),
-  borderAcc: createPos({x: 0.05, y: 0.1}),
-  trys: 0
-};
-
-const out = {
-  accuracy: 38,
-  accColor: 'red',
-  precision: 'okay',
-  precColor: 'green',
-  message: 'Acc is fine',
-  proceedBtnActivated: false,
-};
-
 const createCalcScoreInp = ({
   xAcc, yAcc, xPrec, yPrec, trys
 }) => ({
@@ -103,96 +46,241 @@ const createCalcScoreInp = ({
   trys
 });
 
-const createCalcScoreEvalOut = ({
-  inpScore,
-  accColor,
-  precStatus,
-  precStatusColor,
-  message,
-  proceedBtnActivated
-}) => ({
-  accScore: calcCalibrationScore({
-    acc: inpScore.relAcc,
-    borderAcc: inpScore.borderAcc,
-    borderPrec: inpScore.borderPrec
-  }),
-  accColor,
-  precStatus
-  precStatusColor,
-  message,
-  proceedBtnActivated
-});
-
-
 test('Insufficent Precision', () => {
   const trys = {trys: 0};
-  const lowYPrecisionHighAccuracy = createCalcScoreInpt({
+  const lowYPrecisionHighAccuracy = createCalcScoreInp({
     xAcc: 0.01, yAcc: 0.02, xPrec: 0.01, yPrec: 0.2, trys
   });
-  const firstTryNotAbleToProceed = createCalcScoreEvalOut = ({
-    inpScore: lowYPrecisionHighAccuracy,
-    accColor: 'green',
+  const firstTryNotAbleToProceed = createCalcScoreEvalOut({
+    accScore: createPos({x: 100, y: 100}),
+    accScoreColor: 'green',
     precStatus: 'bad',
     precStatusColor: 'red',
     message:
-      'Your precision is too low to use the program properly, ' +
-      'please calibrate again.',
-    proceedBtnActivated: false
+      'Your accuracy is good.\n' +
+      'Your precision is too low to use the program properly.\n' +
+      'Please calibrate again.',
+    proceedBtnActive: false
   });
   expect(evaluateCalibrationScore(lowYPrecisionHighAccuracy))
     .toEqual(firstTryNotAbleToProceed);
   expect(trys.trys).toBe(1);
 
-  const lowXPrecisionHighAccuracy = createCalcScoreInpt({
-    xAcc: 0.01, yAcc: 0.02, xPrec: 0.1, yPrec: 0.02, trys
+  const lowXPrecisionHighAccuracy = createCalcScoreInp({
+    xAcc: 0.042, yAcc: 0.084, xPrec: 0.1, yPrec: 0.02, trys
   });
-  const secondTryAbleToProceed = createCalcScoreEvalOut = ({
-    inpScore: lowXPrecisionHighAccuracy,
-    accColor: 'green',
+  const secondTryAbleToProceed = createCalcScoreEvalOut({
+    accScore: createPos({x: 80, y: 80}),
+    accScoreColor: 'green',
     precStatus: 'bad',
     precStatusColor: 'red',
     message:
-      'Your precision is too low to use the program properly, ' +
-      'please calibrate again, or proceed.',
-    proceedBtnActivated: true
+      'Your accuracy is good.\n' +
+      'Your precision is too low to use the program properly.\n' +
+      'Please calibrate again, or proceed with limited quality.',
+    proceedBtnActive: true
   });
   expect(evaluateCalibrationScore(lowXPrecisionHighAccuracy))
     .toEqual(secondTryAbleToProceed);
   expect(trys.trys).toBe(2);
 });
 
-test('Relative Accuracy is lower than 50% in X or Y')
+test('Relative Accuracy is lower than 50% in X or Y', () => {
   const trys = {trys: 0};
-  const lowXAccHighYAccAndPrec = createCalcScoreInpt({
-    xAcc: 0.07, yAcc: 0.02, xPrec: 0.01, yPrec: 0.02, trys
+  const lowXAccHighPrec = createCalcScoreInp({
+    xAcc: 0.0606, yAcc: 0.02, xPrec: 0.01, yPrec: 0.02, trys
   });
-  const firstTryNotAbleToProceed = createCalcScoreEvalOut = ({
-    inpScore: lowPrecisionHighAccuracy,
-    accColor: 'green',
-    precStatus: 'bad',
-    precStatusColor: 'red',
+  const firstTryNotAbleToProceed = createCalcScoreEvalOut({
+    accScore: createPos({x: 49, y: 100}),
+    accScoreColor: 'red',
+    precStatus: 'good',
+    precStatusColor: 'green',
     message:
-      'Your precision is too low to use the program properly, ' +
-      'please calibrate again.',
-    proceedBtnActivated: false
+      'Your accuracy is too low to use the program properly.\n' +
+      'Please calibrate again.',
+    proceedBtnActive: false
   });
-  expect(evaluateCalibrationScore(lowPrecisionHighAccuracy))
-    .toEqual(firstRes);
+  expect(evaluateCalibrationScore(lowXAccHighPrec))
+    .toEqual(firstTryNotAbleToProceed);
   expect(trys.trys).toBe(1);
 
-  const secondRes = createCalcScoreEvalOut = ({
-    inpScore: lowPrecisionHighAccuracy,
-    accColor: 'green',
+  const lowYAccHighPrec = createCalcScoreInp({
+    xAcc: 0.02, yAcc: 0.1212, xPrec: 0.01, yPrec: 0.02, trys
+  });
+  const secondTryAbleToProceed = createCalcScoreEvalOut({
+    accScore: createPos({x: 100, y: 49}),
+    accScoreColor: 'red',
+    precStatus: 'good',
+    precStatusColor: 'green',
+    message:
+      'Your accuracy is too low to use the program properly.\n' +
+      'Please calibrate again, or proceed with limited quality.',
+    proceedBtnActive: true
+  });
+  expect(evaluateCalibrationScore(lowYAccHighPrec))
+    .toEqual(secondTryAbleToProceed);
+  expect(trys.trys).toBe(2);
+});
+
+test('Accuracy and Precision too low', () => {
+  const trys = {trys: 0};
+  const lowXAccHighPrec = createCalcScoreInp({
+    xAcc: 0.0606, yAcc: 0.1212, xPrec: 0.1, yPrec: 0.02, trys
+  });
+  const firstTryNotAbleToProceed = createCalcScoreEvalOut({
+    accScore: createPos({x: 49, y: 49}),
+    accScoreColor: 'red',
     precStatus: 'bad',
     precStatusColor: 'red',
     message:
-      'Your precision is too low to use the program properly, ' +
-      'please calibrate again, or proceed.',
-    proceedBtnActivated: true
+      'Your accuracy is too low to use the program properly.\n' +
+      'Your precision is too low to use the program properly.\n' +
+      'Please calibrate again.',
+    proceedBtnActive: false
   });
-  expect(evaluateCalibrationScore(lowPrecisionHighAccuracy))
-    .toEqual(firstTry);
+  expect(evaluateCalibrationScore(lowXAccHighPrec))
+    .toEqual(firstTryNotAbleToProceed);
+  expect(trys.trys).toBe(1);
+
+  const lowYAccHighPrec = createCalcScoreInp({
+    xAcc: 0.02, yAcc: 0.1212, xPrec: 0.01, yPrec: 0.2, trys
+  });
+  const secondTryAbleToProceed = createCalcScoreEvalOut({
+    accScore: createPos({x: 100, y: 49}),
+    accScoreColor: 'red',
+    precStatus: 'bad',
+    precStatusColor: 'red',
+    message:
+      'Your accuracy is too low to use the program properly.\n' +
+      'Your precision is too low to use the program properly.\n' +
+      'Please calibrate again, or proceed with limited quality.',
+    proceedBtnActive: true
+  });
+  expect(evaluateCalibrationScore(lowYAccHighPrec))
+    .toEqual(secondTryAbleToProceed);
   expect(trys.trys).toBe(2);
 });
+
+test('Precision and Accuracy are just enough', () => {
+  const trys = {trys: 0};
+  const medAccMedPrec = createCalcScoreInp({
+    xAcc: 0.06, yAcc: 0.12, xPrec: 0.06, yPrec: 0.1, trys
+  });
+  const firstTryAbleToProceed = createCalcScoreEvalOut({
+    accScore: createPos({x: 50, y: 50}),
+    accScoreColor: 'orange',
+    precStatus: 'good',
+    precStatusColor: 'green',
+    message:
+      'Your accuracy is just enough to use the program.\n' +
+      'If you want an better experience, try to calibrate again.',
+    proceedBtnActive: true
+  });
+  expect(evaluateCalibrationScore(medAccMedPrec))
+    .toEqual(firstTryAbleToProceed);
+  expect(trys.trys).toBe(1);
+});
+
+test('Accuracy is 65% and precision okay', () => {
+  const trys = {trys: 0};
+  const goodAccMedPrec = createCalcScoreInp({
+    xAcc: 0.051, yAcc: 0.102, xPrec: 0.06, yPrec: 0.1, trys
+  });
+  const firstTryAbleToProceed = createCalcScoreEvalOut({
+    accScore: createPos({x: 65, y: 65}),
+    accScoreColor: 'yellow',
+    precStatus: 'good',
+    precStatusColor: 'green',
+    message:
+      'Your accuracy is good enough to use the program.\n' +
+      'If you want an better experience, try to calibrate again.',
+    proceedBtnActive: true
+  });
+  expect(evaluateCalibrationScore(goodAccMedPrec))
+    .toEqual(firstTryAbleToProceed);
+  expect(trys.trys).toBe(1);
+});
+
+test('Accuracy is 80% and precision okay', () => {
+  const trys = {trys: 0};
+  const veryGoodAccMedPrec = createCalcScoreInp({
+    xAcc: 0.042, yAcc: 0.084, xPrec: 0.06, yPrec: 0.1, trys
+  });
+  const firstTryAbleToProceed = createCalcScoreEvalOut({
+    accScore: createPos({x: 80, y: 80}),
+    accScoreColor: 'green',
+    precStatus: 'good',
+    precStatusColor: 'green',
+    message: 'Your accuracy is good.',
+    proceedBtnActive: true
+  });
+  expect(evaluateCalibrationScore(veryGoodAccMedPrec))
+    .toEqual(firstTryAbleToProceed);
+  expect(trys.trys).toBe(1);
+});
+
+/*
+ * pct table
+ *
+ * 1pct x: 0,0006
+ * 1pct y: 0,0012
+ *
+ * 65, 65: createPos({x: 0.051, y: 0.102})
+ * 80, 80 createPos({x: 0.042, x: 0.084})
+ *createPos({x: 0.03, y: 0.06}) : 100, 100
+  createPos({x: 0.02, y: 0.05}) : 100, 100
+  createPos({x: 0.1, y: 0.19})  : 0, 0
+  createPos({x: 0.06, y: 0.12}) : 50, 50
+  createPos({x: 0.045, y: 0.09) : 75, 75
+  createPos({x: 0.075, y: 0.15) : 25, 25
+ * test cases : precision not fullfiled deny first, works for second,
+ *              acc not fullfiled deny first, works for second,
+ *              both at edge fullfiled works,
+ *              acc >= 65% fullfilled,
+ *              acc >= 80% fullfilled
+ *
+ */
+
+
+/*
+ *
+ * Success score for the calibration
+ *
+ * Give a score from 0% to 100% to the calculated accuracy aftter ones
+ * calibration.
+ *
+ * 50% means: acc.x = 0.06; acc.y = 0,12
+ *
+ * For the precision one can either fail, or succed
+ * Sucession means that the precision is in x direction >= 0.07, and
+ *  in y-direction >= 0.08
+ *
+ * Depending on the score a sentence is displed on a screen that indiacates
+ * how good the calibration was. And a color indicates that too.
+ *
+ * If ones score is higher or equals 50% one can proceed immeditely,
+ * but can also retry the calibration.
+ * If one scores less than 50% one have to repeat the calibration one time
+ * before being able to prceed.
+ *
+ *
+ * test cases : precision not fullfiled deny first, works for second,
+ *              acc not fullfiled deny first, works for second,
+ *              both at edge fullfiled works,
+ *              acc >= 65% fullfilled,
+ *              acc >= 80% fullfilled
+ * Scenarios:
+ * X or Y is under 50% or Precision is insufficient
+ *   -> score or precision in red
+ *   -> The next time one can proceed, but with warning message if criterias
+ *      are not fullfilled;
+ *   -> Can proceed without warning message, when criterias are fullfilled.
+ * 
+ * X or Y is 50% or more and Precision is sufficient
+ *   -> Precision in green, show: Precision: good
+ *   -> 50% - 59%: orange, message: Accuracy is sufficient.
+ *   -> 60% to 75%: yellow, message: Good accuracy.
+ *   -> 75% to 100%: green, message: Very good accuracy.
+ */
 
 
